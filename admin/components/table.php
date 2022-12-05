252d5f7e23facba1527f264c32f90a831cd0c97b
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-05 11:45:39 +0100

    add dynamic table columns and rows - completed

--- admin/components/table.php
+++ admin/components/table.php
@@ -3,10 +3,7 @@
               <thead>
                 <tr>
                   <?php foreach ($pages['teams']['table']['columns'] as $column) { ?>
-                    <!-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Id</th>
-                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th> -->
-                    <th class="<?php if($column != "NAME"){echo "text-center ";} ?>text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"><?= $column ?></th>
-                    <!-- <th class="text-secondary opacity-7"></th> -->
+                    <th class="<?php if($column != "NAME" && $column != "ACTION"){echo "text-center ";} ?>text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"><?= $column ?></th>
                   <?php } ?>
                 </tr>
               </thead>
@@ -29,7 +26,7 @@
                     </td>
                   
                     <td class="align-middle text-center">
-                      <span class="text-secondary text-xs font-weight-bold">Morocco</span>
+                      <span class="text-secondary text-xs font-weight-bold"><?= $row['country']?></span>
                     </td>
                     <td class="align-middle">
                       <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
@@ -38,55 +35,5 @@
                     </td>
                   </tr>
                 <?php } ?>
-                <!-- <tr>
-                  <td>
-                    <p class="text-center text-xs text-secondary mb-0">#2</p>
-                  </td>
-                  <td>
-                    <div class="d-flex px-2 py-1">
-                      <div>
-                        <img src="../assets/img/teams/Canada.png" class="avatar avatar-sm me-3" alt="user2">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="mb-0 text-sm">Canada men's national soccer team</h6>
-                        <p class="text-xs text-secondary mb-0"></p>
-                      </div>
-                    </div>
-                  </td>
-                
-                  <td class="align-middle text-center">
-                    <span class="text-secondary text-xs font-weight-bold">Canada</span>
-                  </td>
-                  <td class="align-middle">
-                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <p class="text-center text-xs text-secondary mb-0">#3</p>
-                  </td>
-                  <td>
-                    <div class="d-flex px-2 py-1">
-                      <div>
-                        <img src="../assets/img/teams/Argentina.png" class="avatar avatar-sm me-3" alt="user3">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="mb-0 text-sm">Argentina national football team</h6>
-                        <p class="text-xs text-secondary mb-0">La Albiceleste (The White and Sky Blue)</p>
-                      </div>
-                    </div>
-                  </td>
-              
-                  <td class="align-middle text-center">
-                    <span class="text-secondary text-xs font-weight-bold">Argentina</span>
-                  </td>
-                  <td class="align-middle">
-                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                      Edit
-                    </a>
-                  </td>
-                </tr> -->
               </tbody>
             </table>
