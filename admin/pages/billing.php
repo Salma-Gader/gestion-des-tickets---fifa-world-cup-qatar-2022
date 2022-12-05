fb979e7a904a9b3a19b7556618b1a9d4fa45d169
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-05 12:12:35 +0100

    change billing.php file structur

--- admin/pages/billing.php
+++ admin/pages/billing.php
@@ -2,16 +2,16 @@
     <div class="container-fluid py-4">
       <div class="row">
         <div class="col-12">
-          <div class="card mb-4">
-            <div class="card-header pb-0">
-              <h6>Teams</h6>
-            </div>
-            <div class="card-body px-0 pt-0 pb-2">
-              <div class="table-responsive p-0">
-                <?php include'../components/table.php' ?>
+            <div class="card mb-4">
+              <div class="card-header pb-0">
+                <h6><?= $pages['teams']['title'] ?></h6>
+              </div>
+              <div class="card-body px-0 pt-0 pb-2">
+                <div class="table-responsive p-0">
+                  <?php include'../components/table.php' ?>
+                </div>
               </div>
             </div>
-          </div>
         </div>
       </div>
 <?php include'../layouts/footer.php' ?>
