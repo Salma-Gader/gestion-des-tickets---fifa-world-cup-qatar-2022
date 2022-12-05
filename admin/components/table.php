53f0d013fe8ff80d18d54358fa80616628ed109b
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-05 15:46:58 +0100

    add deynamic teams and stadiums dashboard type - completed

--- admin/components/table.php
+++ admin/components/table.php
@@ -2,38 +2,71 @@
             <table class="table align-items-center mb-0">
               <thead>
                 <tr>
-                  <?php foreach ($pages['teams']['table']['columns'] as $column) { ?>
+                  <?php foreach ($page['table']['columns'] as $column) { ?>
                     <th class="<?php if($column != "NAME" && $column != "ACTION"){echo "text-center ";} ?>text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"><?= $column ?></th>
                   <?php } ?>
                 </tr>
               </thead>
               <tbody>
-                <?php foreach ($pages['teams']['table']['row'] as $row) { ?>
-                  <tr>
-                    <td>
-                      <p class="text-center text-xs text-secondary mb-0">#<?= $row['id']?></p>
-                    </td>
-                    <td>
-                      <div class="d-flex px-2 py-1">
-                        <div>
-                          <img src="../assets/img/teams/<?= $row['image']?>" class="avatar avatar-sm me-3" alt="user1">
+                <?php if($_GET['type'] == "teams"){ 
+                  foreach ($page['table']['row'] as $row) { ?>
+                    <tr>
+                      <td>
+                        <p class="text-center text-xs text-secondary mb-0">#<?= $row['id']?></p>
+                      </td>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="../assets/img/teams/<?= $row['image']?>" class="avatar avatar-sm me-3" alt="user1">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm"><?= $row['name']?></h6>
+                            <p class="text-xs text-secondary mb-0"><?= $row['aka']?></p>
+                          </div>
                         </div>
-                        <div class="d-flex flex-column justify-content-center">
-                          <h6 class="mb-0 text-sm"><?= $row['name']?></h6>
-                          <p class="text-xs text-secondary mb-0"><?= $row['aka']?></p>
+                      </td>
+                    
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold"><?= $row['country']?></span>
+                      </td>
+                      <td class="align-middle">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Edit
+                        </a>
+                      </td>
+                    </tr>
+                <?php }
+                  }elseif($_GET['type'] == "stadiums"){ ?>
+                  <?php foreach ($page['table']['row'] as $row) { ?>
+                    <tr>
+                      <td>
+                        <p class="text-center text-xs text-secondary mb-0">#<?= $row['id'] ?></p>
+                      </td>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="../assets/img/all/<?= $row['image'] ?>" class="avatar avatar-xxl me-3" alt="user1">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm"><?= $row['name'] ?></h6>
+                            <p class="text-xs text-secondary mb-0"><?= $row['description'] ?></p>
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                  
-                    <td class="align-middle text-center">
-                      <span class="text-secondary text-xs font-weight-bold"><?= $row['country']?></span>
-                    </td>
-                    <td class="align-middle">
-                      <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
+                      </td>
+                      <td>
+                        <p class="text-xs text-secondary mb-0"><?= $row['location'] ?></p>
+                      </td>
+                    
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold"><?= $row['capacity'] ?></span>
+                      </td>
+                      <td class="align-middle">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Edit
+                        </a>
+                      </td>
+                    </tr>
+                  <?php } ?>
                 <?php } ?>
               </tbody>
             </table>
