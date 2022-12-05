9aad2a7e5652ec7847a103a1514205ef49406fce
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-05 16:14:02 +0100

    edit in data.php file structur

--- admin/pages/data.php
+++ admin/pages/data.php
@@ -1,49 +1,51 @@
 <?php
 $pages = [
     'teams' =>[
-                'title' => 'Teams',
-                'table' => [
-                    'columns' => ['ID' ,'NAME' , 'COUNTRY' , 'ACTION'],
-                    'row' => [
-                        [
-                            'id' => 1,
-                            'image' => "Morocco.png",
-                            'name' => "Morocco national football team",
-                            'aka' => "The Atlas Lions",
-                            'country' => "Morocco",
-                        ],
-                        [
-                            'id' => 2,
-                            'image' => "Canada.png",
-                            'name' => "Canada men's national soccer team",
-                            'aka' => "",
-                            'country' => "Canada"
-                        ],
-                    ]
+        'title' => 'Teams',
+        'table' => [
+            'columns' => ['ID' ,'NAME' , 'COUNTRY' , 'ACTION'],
+            'row' => [
+                [
+                    'id' => 1,
+                    'image' => "Morocco.png",
+                    'name' => "Morocco national football team",
+                    'aka' => "The Atlas Lions",
+                    'country' => "Morocco",
                 ],
-            ],
+                [
+                    'id' => 2,
+                    'image' => "Canada.png",
+                    'name' => "Canada men's national soccer team",
+                    'aka' => "",
+                    'country' => "Canada"
+                ],
+            ]
+        ],
+    ],
     'stadiums' => [
-                'title' => 'Stadiums',
-                'columns' => ['ID' , 'image' , 'NAME' , 'LOCATION' , 'CAPACITY' , 'ACTION'],
-                'data' => [
-                    [
-                        'id' => 1,
-                        'image' => "al-bayt.png",
-                        'name' => "Al Bayt Stadium",
-                        'mini-description' => "Enjoy the warmest of Arab welcomes",
-                        'location' => "Al Khor City, 35km north of Doha",
-                        'capacity' => 68,895
-                    ],
-                    [
-                        'id' => 2,
-                        'image' => "lusail.png",
-                        'name' => "Lusail Stadium",
-                        'description' => "Alive with heritage, an icon for the future",
-                        'location' => "Lusail City, 20km north of central Doha",
-                        'capacity' => 88,966
-                    ],
-                ]
-            ],
+        'title' => 'Stadiums',
+        'table' => [
+            'columns' => ['ID' , 'NAME' , 'LOCATION' , 'CAPACITY' , 'ACTION'],
+            'row' => [
+                [
+                    'id' => 1,
+                    'image' => "al-bayt.png",
+                    'name' => "Al Bayt Stadium",
+                    'description' => "Enjoy the warmest of Arab welcomes",
+                    'location' => "Al Khor City, 35km north of Doha",
+                    'capacity' => 68,895
+                ],
+                [
+                    'id' => 2,
+                    'image' => "lusail.png",
+                    'name' => "Lusail Stadium",
+                    'description' => "Alive with heritage, an icon for the future",
+                    'location' => "Lusail City, 20km north of central Doha",
+                    'capacity' => 88,966
+                ],
+            ]
+        ]
+    ],
 ]
 
 ?>
