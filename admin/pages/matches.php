fb25eb054484598f0065a0654bba938499c5cca8
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-07 11:51:09 +0100

    add matches table and model form - workout

--- admin/pages/matches.php
+++ admin/pages/matches.php
@@ -1,3 +1,81 @@
 <?php include'../layouts/header.php'; $page = $pages['matches']; ?>
-  <?php include'../components/table.php' ?>
+<div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+              <div class="card-header pb-0">
+                <h6><?= $page['title'] ?></h6>
+              </div>
+              <div class="container"><!-- Button trigger modal -->
+                <button type="button" class="btn btn-sm btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Add Matche
+                </button>
+              </div>
+              <div class="card-body px-0 pt-0 pb-2">
+                <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                    <thead>
+                      <tr>
+                        <?php foreach ($page['table']['columns'] as $column) { ?>
+                          <th class="<?php if($column != "NAME" && $column != "ACTION"){echo "text-center ";} ?>text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"><?= $column ?></th>
+                        <?php } ?>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($page['table']['row'] as $row) { ?>
+                          <tr>
+                            <td>
+                              <p class="text-center text-xs text-secondary mb-0">#<?= $row['id'] ?></p>
+                            </td>
+                            <td>
+                              <div class="d-flex px-2 py-1">
+                                <div>
+                                  <img src="../assets/img/all/<?= $row['first_team']['image'] ?>" class="avatar avatar-sm me-3" alt="user1">
+                                  <img src="../assets/img/all/<?= $row['secound_team']['image'] ?>" class="avatar avatar-sm me-3" alt="user1">
+                                </div>
+                                <div class="d-flex flex-column justify-content-center">
+                                  <h6 class="mb-0 text-sm"><?= $row['first_team']['name'] ?> / </h6>
+                                  <h6 class="mb-0 text-sm"><?= $row['secound_team']['name'] ?></h6>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              <p class="text-xs text-secondary mb-0"><?= $row['date'] ?></p>
+                            </td>
+                            <td>
+                              <p class="text-xs text-secondary mb-0"><?= $row['stadium']['name'] ?></p>
+                            </td>
+                            <td class="align-middle">
+                              <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                                Edit
+                              </a>
+                            </td>
+                          </tr>
+                        <?php } ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include'../layouts/footer.php' ?>
