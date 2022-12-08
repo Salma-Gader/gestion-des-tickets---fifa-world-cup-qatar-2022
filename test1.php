1ae262f1277457adf9d22ee3e40665d3b07a84fb
Salma-Gader <salmagader@gmail.com>
2022-12-08 16:13:59 +0100

    for switch branch

--- test1.php
+++ test1.php
@@ -16,17 +16,6 @@ function reserve(){
     header('location:reservation.php');
 }
 
-
-function saveData(){
-    $insert = new crud();
-    $name=$_POST["name"];
-    $description=$_POST["description"];
-    $capacity=$_POST["capacity"];
-    $location=$_POST["location"];
-    $data=[$name,$description,$capacity,$location];
-    $insert->action("INSERT INTO stadiums(name,description,capacity,location) VALUES(?,?,?,?)",$data);
-    header("Location: ../admin/pages/stadiums.php");
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
