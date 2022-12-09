2958d71887e281a7aca1efe1605f7102c9ed7239
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-09 16:47:52 +0100

    add matches value in matches view

--- admin/pages/matches.php
+++ admin/pages/matches.php
@@ -1,4 +1,10 @@
-<?php include'../layouts/header.php'; $page = $pages['matches']; ?>
+<?php
+require '../../controllers/Matches.php';
+include'../layouts/header.php';
+$page = $pages['matches'];
+$matches = new Matche();
+$result = $matches->show();
+?>
 <div class="container-fluid py-4">
       <div class="row">
         <div class="col-12">
@@ -24,7 +30,7 @@
                       </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($page['table']['row'] as $row) { ?>
+                        <?php foreach ($result as $row) { ?>
                           <tr>
                             <td>
                               <p class="text-center text-xs text-secondary mb-0">#<?= $row['id'] ?></p>
@@ -32,12 +38,12 @@
                             <td>
                               <div class="d-flex px-2 py-1">
                                 <div>
-                                  <img src="../assets/img/all/<?= $row['first_team']['image'] ?>" class="avatar avatar-sm me-3" alt="user1">
-                                  <img src="../assets/img/all/<?= $row['secound_team']['image'] ?>" class="avatar avatar-sm me-3" alt="user1">
+                                  <img src="../assets/img/all/<?= $row['first_team_image'] ?>" class="avatar avatar-sm me-3" alt="user1">
+                                  <img src="../assets/img/all/<?= $row['second_team_image'] ?>" class="avatar avatar-sm me-3" alt="user1">
                                 </div>
                                 <div class="d-flex flex-column justify-content-center">
-                                  <h6 class="mb-0 text-sm"><?= $row['first_team']['name'] ?> / </h6>
-                                  <h6 class="mb-0 text-sm"><?= $row['secound_team']['name'] ?></h6>
+                                  <h6 class="mb-0 text-sm"><?=  $row['first_team_name'] ?> / </h6>
+                                  <h6 class="mb-0 text-sm"><?=  $row['second_team_name'] ?></h6>
                                 </div>
                               </div>
                             </td>
@@ -45,7 +51,7 @@
                               <p class="text-xs text-secondary mb-0"><?= $row['date'] ?></p>
                             </td>
                             <td>
-                              <p class="text-xs text-secondary mb-0"><?= $row['stadium']['name'] ?></p>
+                              <p class="text-xs text-secondary mb-0"><?= $row['stadium_name'] ?></p>
                             </td>
                             <td class="align-middle">
                               <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
