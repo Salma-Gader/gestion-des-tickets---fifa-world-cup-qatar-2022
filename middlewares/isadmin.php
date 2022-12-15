515cdf19ceec99c10ef2c260052a03c268b2550d
spirithuntt <nouhaila.elhousnii@gmail.com>
2022-12-15 14:18:15 +0100

    updated admin role

--- middlewares/isadmin.php
+++ middlewares/isadmin.php
@@ -3,10 +3,9 @@ class IsAdmin
 {
     public function __construct()
     {
+     // if the url has admin in it redirect to index.php
         if ($_SESSION['isadmin'] == 0) {
             if (strpos($_SERVER['PHP_SELF'], 'admin') !== false) {
-                // if the url has admin in it redirect to index.php
-                // redirect to index.php in the root folder
                 header('location: ../../index.php');
             }
         }
